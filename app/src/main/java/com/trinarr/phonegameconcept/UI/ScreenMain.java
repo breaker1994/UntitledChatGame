bb471352ec2026e738b1fb134c18045c96b426fe
breaker1994 <nlukanin@c-cars.tech>
2020-01-31 15:54:22 +0300

    Reworked main screen

--- app/src/main/java/com/trinarr/phonegameconcept/UI/ScreenMain.java
+++ app/src/main/java/com/trinarr/phonegameconcept/UI/ScreenMain.java
@@ -1,73 +1,78 @@
 package com.trinarr.phonegameconcept.UI;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
 
-import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.trinarr.phonegameconcept.R;
 
-public class ScreenTabs extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class ScreenMain extends AppCompatActivity implements ListAdapterChats.OnItemClickListener{
+    private ArrayList<ListItemChats> graphlistArray = new ArrayList<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_screen_tabs);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(0);
-        menuItem.setChecked(true);
+        BlockHeader blockHeader = new BlockHeader(this);
+        LinearLayout parentLayout = findViewById(R.id.parentLayout);
+        parentLayout.addView(blockHeader, 0);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                openScreen(menuItem.getItemId());
-                return false;
-            }
-        });
+        blockHeader.setHeaderText("Чаты");
 
-        openScreen(R.id.tab_chats);
+        RecyclerView graphList = findViewById(R.id.recycleView);
+        ListAdapterChats graphListAdapter = new ListAdapterChats(this, graphList, graphlistArray, this);
+
+        graphList.addOnItemTouchListener(graphListAdapter);
+        graphList.setAdapter(graphListAdapter);
+
+        graphList.addItemDecoration(new DividerItemDecoration(this, DividerItemDecoration.VERTICAL));
+        graphList.setLayoutManager(new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false));
+
+        ListItemChats item = new ListItemChats("Сергей Сергеевич", "Соберемся, ящас хочу чуть отдохнуть, а то вообще че то уже каждый день ебошу аж лететь перестало!)");
+        graphlistArray.add(item);
+        graphlistArray.add(item);
+        graphlistArray.add(item);
+        graphlistArray.add(item);
     }
 
-    private void openScreen(int type) {
-        FragmentManager fragmentManager = getSupportFragmentManager();
+    @Override public void onItemClick(View view, int position) {
+        LogManager.log("onItemClick  "+position, this.getClass());
 
-        Fragment fragment;
-        int numType;
+        Intent intent = new Intent(this, ScreenChat.class);
+        startActivity(intent);
+    }
 
-        switch (type) {
-            default:
-            case R.id.tab_chats:
-                numType = 0;
-                fragment = new TabChats();
-                break;
-            case R.id.tab_settings:
-                numType = 1;
-                return;
-                //fragment = new TabNews();
-                //break;
+    @Override public void onLongItemClick(View view, int position) {
+        LogManager.log("onLongItemClick  "+position, this.getClass());
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.toolbar_settings) {
+            //Intent intent = new Intent(ScreenGeofence.this, ScreenGeozone.class);
+            //startActivity(intent);
         }
+        return true;
+    }
 
-        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        if(fragmentManager.getFragments().size() == 0) {
-            fragmentTransaction.add(R.id.frameLayout, fragment);
-        }
-        else {
-            fragmentTransaction.replace(R.id.frameLayout, fragment);
-        }
-        fragmentTransaction
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
-                .commit();
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.navigation_toolbar, menu);
 
-        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        Menu menu = bottomNavigationView.getMenu();
-        MenuItem menuItem = menu.getItem(numType);
-        menuItem.setChecked(true);
+        MenuItem menuItem = menu.findItem(R.id.toolbar_settings);
+        menuItem.setVisible(true);
+
+        return true;
     }
 }
